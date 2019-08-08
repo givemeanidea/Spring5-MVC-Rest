c70c2c48249d45edff0b7a0136646bcf3e3d7fa1
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-08 16:16:22 +0100

    Added category service

---
+++ src/main/java/com/springframework/services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.springframework.services;
+
+import com.springframework.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
