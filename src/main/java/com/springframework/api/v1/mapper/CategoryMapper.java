fb922d48da99d27ec7f524cd95b4796b8e8df859
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-08 15:29:53 +0100

    Implemented MapStruct

---
+++ src/main/java/com/springframework/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.springframework.api.v1.mapper;
+
+import com.springframework.api.v1.model.CategoryDTO;
+import com.springframework.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
--- src/main/java/com/springframework/api/v1/model/CategoryDTO.java
+++ src/main/java/com/springframework/api/v1/model/CategoryDTO.java
@@ -1,5 +1,8 @@
 package com.springframework.api.v1.model;
 
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
 
     private Long id;
