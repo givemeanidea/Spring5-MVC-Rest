f91186d68ef6473098f4441420a2d41aca46560d
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-09 14:19:59 +0100

    Added vendor Mapper

---
+++ src/main/java/com/springframework/api/v1/mapper/VendorMapper.java
@@ -0,0 +1,16 @@
+package com.springframework.api.v1.mapper;
+
+import com.springframework.api.v1.model.VendorDTO;
+import com.springframework.domain.Vendor;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface VendorMapper {
+
+    VendorMapper INSTANCE = Mappers.getMapper(VendorMapper.class);
+
+    VendorDTO vendorToVendorDTO(Vendor vendor);
+
+    Vendor vendorDtoToVendor(VendorDTO vendorDTO);
+}
