8689a4943daed712e63fc5321dd1486326ddead3
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-09 14:33:39 +0100

    Added VendorService interface

---
+++ src/main/java/com/springframework/services/VendorService.java
@@ -0,0 +1,20 @@
+package com.springframework.services;
+
+import com.springframework.api.v1.model.VendorDTO;
+
+import java.util.List;
+
+public interface VendorService {
+
+    List<VendorDTO> getAllVendors();
+
+    VendorDTO getVendorById(Long id);
+
+    VendorDTO createNewVendor(VendorDTO vendorDTO);
+
+    VendorDTO saveVendoByDTO(Long id, VendorDTO vendorDTO);
+
+    VendorDTO patchVendor(Long id, VendorDTO vendorDTO);
+
+    void deleteVendorById(Long id);
+}
