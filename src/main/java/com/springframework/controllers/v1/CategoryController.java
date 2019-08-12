416eb9b6329bad3f7d6c13a22e238119b80a7b21
Daniel <byrne.dan01@gmail.com>
2019-08-12 13:09:02 +0100

    Amended API docs

--- src/main/java/com/springframework/controllers/v1/CategoryController.java
+++ src/main/java/com/springframework/controllers/v1/CategoryController.java
@@ -4,6 +4,7 @@ import com.springframework.api.v1.model.CategoryDTO;
 import com.springframework.api.v1.model.CategoryListDTO;
 import com.springframework.services.CategoryService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,12 +24,14 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @ApiOperation(value = "View list of Categories")
     @GetMapping
     public ResponseEntity<CategoryListDTO> getAllCategories() {
         return new ResponseEntity<>
                  (new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get a Category by name")
     @GetMapping("{name}")
     public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
         return new ResponseEntity<>(
--- src/main/java/com/springframework/controllers/v1/CustomerController.java
+++ src/main/java/com/springframework/controllers/v1/CustomerController.java
@@ -4,6 +4,7 @@ import com.springframework.api.v1.model.CustomerDTO;
 import com.springframework.api.v1.model.CustomerListDTO;
 import com.springframework.services.CustomerService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -21,31 +22,37 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "Get a list of all Customers")
     @GetMapping
     public ResponseEntity<CustomerListDTO> getAllCustomers() {
         return new ResponseEntity<>(new CustomerListDTO(customerService.getAllCustomers()), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get Customer by ID")
     @GetMapping("/{id}")
     public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id) {
         return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.OK);
 }
 
+    @ApiOperation(value = "Create a new Customer")
     @PostMapping
     public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO) {
         return new ResponseEntity<>(customerService.createNewCustomer(customerDTO), HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Update an existing Customer")
     @PutMapping({"/{id}"})
     public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
         return new ResponseEntity<>(customerService.saveCustomerByDTO(id, customerDTO), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Ammend attribute of Customer")
     @PatchMapping({"/{id}"})
     public ResponseEntity<CustomerDTO> patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
         return new ResponseEntity<>(customerService.patchCustomer(id, customerDTO), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Delete Customer by ID")
     @DeleteMapping({"/{id}"})
     public ResponseEntity<Void> deleteCustomer(@PathVariable Long id){
 
